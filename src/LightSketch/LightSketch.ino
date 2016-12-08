b0694efdfc36e66e591716c5f76a05970ad9cbcd
bjohan <sa6bxq>
2016-12-08 20:26:46 +0100

    Added code for reading dallas

--- src/LightSketch/LightSketch.ino
+++ src/LightSketch/LightSketch.ino
@@ -1,9 +1,12 @@
 #include <EEPROM.h>
+
 extern "C"
 {
 #include "uart_command_lib.h"
 #include "corbomite.h"
 }
+
+
 int ch1TempPin = A0;
 int chargePumpPin = 3;
 int heaterPin = 7;
@@ -83,10 +86,71 @@ const CorbomiteEntry * const entries[] PROGMEM = {
 	&initcmd, &last
 };
 
+} //extern "C"
 
+void resetDallas(int pin){
 
-
+	pinMode(pin, OUTPUT);
+	digitalWrite(pin,0);
+	delayMicroseconds(500);
+	pinMode(pin, INPUT);
 }
+
+void initDallas(int pin)
+{
+	resetDallas(pin);
+	delayMicroseconds(500); //ignore precens pulses for now
+}
+
+void writeBit(int pin, uint8_t bit)
+{
+	noInterrupts();
+	pinMode(pin, OUTPUT);
+	if(bit == 0){
+		delayMicroseconds(65);
+		pinMode(pin, INPUT);
+	        delayMicroseconds(5);
+		
+	} else {
+		delayMicroseconds(10);
+		pinMode(pin, INPUT);
+	        delayMicroseconds(55);
+	}
+	interrupts();
+}
+
+void writeByte(int pin, uint8_t data)
+{
+	for( int i = 0 ; i < 8 ; i++){
+		writeBit(pin, data & 0x01);
+		data = data >> 1;
+	}
+}
+
+
+uint8_t readBit(int pin)
+{
+	uint8_t b;
+	noInterrupts();
+	pinMode(pin, OUTPUT);
+	delayMicroseconds(3);
+	pinMode(pin, INPUT);
+	delayMicroseconds(10);
+	b = digitalRead(pin);
+	interrupts();
+	return b == HIGH ? 0xFF : 0;
+	delayMicroseconds(53);
+}
+
+uint8_t readByte(int pin)
+{
+	uint8_t data;
+	for( int i = 0 ; i < 8 ; i++){
+		data = (data>>1) |(readBit(pin)&+0x80);
+	}
+	return data;
+}
+
 void setup()
 {
   pinMode(ch1TempPin, INPUT);
@@ -102,6 +166,7 @@ void setup()
   //pinMode(heatLed, OUTPUT);
   //pinMode(ch1TempPin, INPUT);
   //pinMode(heaterPin, OUTPUT);
+
 }
 
 
@@ -113,9 +178,68 @@ void printValue(char *name, float value)
   Serial.print(value);
 }
 
+
+int readDallas(int pin){
+	static uint8_t state = 0;
+	static uint32_t t0 = 0;
+	static uint8_t r = 0;
+	static uint16_t t = 0;
+	static uint16_t tr = 0;
+	switch(state){
+		case 0:
+			initDallas(pin);
+			state = 1;
+			break;
+		case 1:
+			writeByte(pin, 0xCC);
+			state = 2;
+			break;
+		case 2:
+			writeByte(pin, 0x44);
+			state = 3;
+			t0 = millis();
+			break;
+		case 3:
+			if (millis() > t0+800){
+				initDallas(pin);
+				state = 4;
+			}
+			break;
+		case 4:
+			writeByte(pin, 0xCC);
+			state = 5;
+			break;
+		case 5:
+			writeByte(pin, 0xBE);
+			state = 6;
+			r = 0;
+			break;
+		case 6:
+			if(r == 0){
+				t = readByte(pin);
+			} else if(r == 1) {
+				tr = t | readByte(pin)<<8;
+			} else {
+				readByte(pin);
+			}
+			if(r==9){
+				state = 0;
+				break;	
+			}
+			r++;
+			break;
+			 
+		default:
+			state = 0;
+	}
+	return tr;
+}
+
+
 void loop()
 {
-  //transmitAnalogIn(&temperature, (int) (readTemperature));
+  Serial.print(float(readDallas(7))/16.0);
+  Serial.println("DERP");
   commandLine();
   if(not inited){
 	  load();
